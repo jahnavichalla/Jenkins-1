128c7b1c4c7daa04c9b0ac302d2c20fe6be19dfa
jahnavichalla <79450805+jahnavichalla@users.noreply.github.com>
2022-11-25 11:43:56 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,8 +7,8 @@ from Prog1 import Addition
 class TestAddition(unittest.TestCase):
     #correct test case
     def test_addition1(self):
-        result = Addition(54,9)
-        self.assertEqual(result,63)
+        result = Addition(1,0)
+        self.assertEqual(result,1)
               
             
 if __name__ == '__main__':
