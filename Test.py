b634545c1d524125bd1c53e1fe432073e58e2fd7
jahnavichalla <79450805+jahnavichalla@users.noreply.github.com>
2022-11-25 11:44:33 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,14 +2,13 @@
 
 import unittest
 from Prog1 import Addition
+    #wrong test case
+    def test_addition1(self):
+        result = Addition(5,4)
+        self.assertEqual(result,1)
 
 
 class TestAddition(unittest.TestCase):
-    #correct test case
-    def test_addition1(self):
-        result = Addition(1,0)
-        self.assertEqual(result,1)
-              
             
 if __name__ == '__main__':
     unittest.main()
