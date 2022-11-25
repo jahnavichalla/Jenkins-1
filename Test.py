d98140c56afa8357b6c6de2e53d83be6da39ac18
jahnavichalla <79450805+jahnavichalla@users.noreply.github.com>
2022-11-25 11:16:41 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,27 +9,7 @@ class TestAddition(unittest.TestCase):
     def test_addition1(self):
         result = Addition(2,3)
         self.assertEqual(result,5)
-
-
-    #correct test case
-    def test_addition1(self):
-        result = Addition(54,9)
-        self.assertEqual(result,63)
-
-    #correct test case
-    def test_addition1(self):
-        result = Addition(1,0)
-        self.assertEqual(result,1)
-
-    #wrong test case
-    def test_addition1(self):
-        result = Addition(5,4)
-        self.assertEqual(result,1)
-
-    #wrong test case
-    def test_addition1(self):
-        result = Addition(1,8)
-        self.assertEqual(result,8)                
+                
 
     if __name__ == '__main__':
         unittest.main()
